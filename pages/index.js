c1e02e46c3c18a7b67a8c5b8c033e4dc8b20068b
vankuver163 <de1uxe87@mail.ru>
2023-10-24 21:40:55 +0400

    Header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "@/components";
-import '../styles/globals.css';
 
 const posts = [
     { title: 'React Testing', excerpt: 'Learn React Testing' },
@@ -10,14 +9,12 @@ const posts = [
 export default function Home() {
     return (
         <div className="container mx-auto px-10 mb-8">
-            <Head>
-                <title>CMS Blog</title>
-                <link rel="icon" href="/favicon.ico"></link>
-            </Head>
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-                {posts.map((post, index) => (
-                    <PostCard key={post.title} post={post} />
-                ))}
+                <div className="lg:col-span-8 col-span-1">
+                    {posts.map((post, index) => (
+                        <PostCard key={post.title} post={post} />
+                    ))}
+                </div>
                 <div className="lg:col-span-4 col-span-1">
                     <div className="lg:sticky relative top-8">
                         <PostWidget />
