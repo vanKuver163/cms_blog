c7b7f0bf9a6f6e3b06a1de112ca1bd43445cd6a0
vankuver163 <de1uxe87@mail.ru>
2023-10-30 21:20:59 +0400

    Comments

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "@/components";
 import { getPosts } from "@/services";
+import { FeaturedPosts } from "@/section";
 
 export default function Home({posts}) {
-    return (
+    return (        
         <div className="container mx-auto px-10 mb-8">
+            <FeaturedPosts />
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                 <div className="lg:col-span-8 col-span-1">
                     {posts.map((post, index) => (
